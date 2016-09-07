fd2c235116573c4f7a175480d5eb27e60595c5fc
Santhosh Kannan <santhosh.kannan@profitbricks.com>
2016-09-07 17:21:48 +0200

    Example updated

--- example/main.tf
+++ example/main.tf
@@ -5,193 +5,214 @@ provider "profitbricks" {
 //Public lan
 resource "profitbricks_lan" "webserver_lan" {
   datacenter_id = "${profitbricks_datacenter.main.id}"
-  public = true
-  name = "public"
+  public        = true
+  name          = "public"
 }
 
 //IP Block
 resource "profitbricks_ipblock" "webserver_ip" {
   location = "${profitbricks_datacenter.main.location}"
-  size = 1
+  size     = 1
 }
 
 //Web Server
 resource "profitbricks_datacenter" "main" {
-  name = "datacenter 01"
-  location = "us/las"
+  name        = "datacenter 01"
+  location    = "us/las"
   description = "description of the datacenter"
 }
 
 resource "profitbricks_server" "webserver" {
-  name = "webserver"
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  cores = 1
-  ram = 1024
+  name              = "webserver"
+  datacenter_id     = "${profitbricks_datacenter.main.id}"
+  cores             = 1
+  ram               = 1024
   availability_zone = "ZONE_1"
-  cpu_family = "AMD_OPTERON"
+  cpu_family        = "AMD_OPTERON"
+
   volume {
-    name = "system"
-    image_name = "${var.ubuntu}"
-    size = 5
-    disk_type = "SSD"
-    ssh_key_path = "${var.private_key_path}"
+    name           = "system"
+    image_name     = "${var.ubuntu}"
+    size           = 5
+    disk_type      = "SSD"
+    ssh_key_path   = ["${var.ssh_keys}"]
     image_password = "test1234"
   }
+
   nic {
-    lan = "${profitbricks_lan.webserver_lan.id}"
-    dhcp = true
-    ip = "${profitbricks_ipblock.webserver_ip.ips}"
+    lan             = "${profitbricks_lan.webserver_lan.id}"
+    dhcp            = true
+    ip              = "${profitbricks_ipblock.webserver_ip.0.ips}"
     firewall_active = true
+
     firewall {
-      protocol = "TCP"
-      name = "SSH"
+      protocol         = "TCP"
+      name             = "SSH"
       port_range_start = 22
-      port_range_end = 22
+      port_range_end   = 22
     }
   }
+
   provisioner "remote-exec" {
     inline = [
-      # install nginx
       "apt-get update",
-      "apt-get -y install nginx"
+      "apt-get -y install nginx",
     ]
+
+    # install nginx
     connection {
-      type = "ssh"
+      type        = "ssh"
       private_key = "${file("${var.private_key_path}")}"
-      user = "root"
-      timeout = "4m"
+      user        = "root"
+      timeout     = "4m"
     }
   }
 }
 
 resource "profitbricks_nic" "webserver_nic" {
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.webserver.id}"
-  lan = 2
-  dhcp = true
+  datacenter_id   = "${profitbricks_datacenter.main.id}"
+  server_id       = "${profitbricks_server.webserver.id}"
+  lan             = 2
+  dhcp            = true
   firewall_active = true
 }
 
 resource "profitbricks_firewall" "webserver_ping" {
   datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.webserver.id}"
-  nic_id = "${profitbricks_server.webserver.primary_nic}"
-  protocol = "ICMP"
-  name = "PING"
-  icmp_type = 8
-  icmp_code = 0
+  server_id     = "${profitbricks_server.webserver.id}"
+  nic_id        = "${profitbricks_server.webserver.primary_nic}"
+  protocol      = "ICMP"
+  name          = "PING"
+  icmp_type     = 8
+  icmp_code     = 0
 }
 
 resource "profitbricks_firewall" "webserver_http" {
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.webserver.id}"
-  nic_id = "${profitbricks_server.webserver.primary_nic}"
-  protocol = "TCP"
-  name = "HTTP"
+  datacenter_id    = "${profitbricks_datacenter.main.id}"
+  server_id        = "${profitbricks_server.webserver.id}"
+  nic_id           = "${profitbricks_server.webserver.primary_nic}"
+  protocol         = "TCP"
+  name             = "HTTP"
   port_range_start = 80
-  port_range_end = 80
+  port_range_end   = 80
 }
 
 resource "profitbricks_firewall" "webserver_https" {
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.webserver.id}"
-  nic_id = "${profitbricks_server.webserver.primary_nic}"
-  protocol = "TCP"
-  name = "HTTPS"
+  datacenter_id    = "${profitbricks_datacenter.main.id}"
+  server_id        = "${profitbricks_server.webserver.id}"
+  nic_id           = "${profitbricks_server.webserver.primary_nic}"
+  protocol         = "TCP"
+  name             = "HTTPS"
   port_range_start = 443
-  port_range_end = 443
+  port_range_end   = 443
 }
 
 //MongoDB Server
 resource "profitbricks_ipblock" "database_ip" {
   location = "${profitbricks_datacenter.main.location}"
-  size = 1
+  size     = 1
 }
 
 resource "profitbricks_server" "database" {
-  name = "mongodb"
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  cores = 1
-  ram = 1024
+  name              = "mongodb"
+  datacenter_id     = "${profitbricks_datacenter.main.id}"
+  cores             = 1
+  ram               = 1024
   availability_zone = "ZONE_1"
-  cpu_family = "INTEL_XEON"
+  cpu_family        = "INTEL_XEON"
+
   volume {
-    name = "system"
-    image_name = "${var.ubuntu}"
-    size = 5
-    disk_type = "HDD"
-    ssh_key_path = "${var.private_key_path}"
+    name           = "system"
+    image_name     = "${var.ubuntu}"
+    size           = 5
+    disk_type      = "HDD"
+    ssh_key_path   = ["${var.ssh_keys}"]
     image_password = "test1234"
   }
+
   nic {
-    lan = "${profitbricks_lan.webserver_lan.id}"
-    dhcp = true
-    ip = "${profitbricks_ipblock.database_ip.ips}"
+    lan             = "${profitbricks_lan.webserver_lan.id}"
+    dhcp            = true
+    ip              = "${profitbricks_ipblock.database_ip.0.ips}"
     firewall_active = true
+
     firewall {
-      protocol = "TCP"
-      name = "SSH"
+      protocol         = "TCP"
+      name             = "SSH"
       port_range_start = 22
-      port_range_end = 22
+      port_range_end   = 22
     }
   }
+
   provisioner "remote-exec" {
     inline = [
       "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927",
       "echo \"deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse\" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list",
       "apt-get update",
-      "apt-get install -y mongodb-org"
+      "apt-get install -y mongodb-org",
     ]
+
     connection {
-      type = "ssh"
+      type        = "ssh"
       private_key = "${file("${var.private_key_path}")}"
-      user = "root"
-      timeout = "4m"
+      user        = "root"
+      timeout     = "4m"
     }
   }
 }
 
 resource "profitbricks_firewall" "database_mongodb" {
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.database.id}"
-  nic_id = "${profitbricks_server.database.primary_nic}"
-  protocol = "TCP"
-  name = "MongoDB"
+  datacenter_id    = "${profitbricks_datacenter.main.id}"
+  server_id        = "${profitbricks_server.database.id}"
+  nic_id           = "${profitbricks_server.database.primary_nic}"
+  protocol         = "TCP"
+  name             = "MongoDB"
   port_range_start = 27017
-  port_range_end = 27017
+  port_range_end   = 27017
 }
 
 resource "profitbricks_firewall" "database_ping" {
   datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.database.id}"
-  nic_id = "${profitbricks_server.database.primary_nic}"
-  protocol = "ICMP"
-  name = "PING"
-  icmp_type = "8"
-  icmp_code = "0"
+  server_id     = "${profitbricks_server.database.id}"
+  nic_id        = "${profitbricks_server.database.primary_nic}"
+  protocol      = "ICMP"
+  name          = "PING"
+  icmp_type     = "8"
+  icmp_code     = "0"
 }
 
 resource "profitbricks_volume" "database_volume" {
   datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.database.id}"
-  licence_type = "OTHER"
-  name = "data"
-  size = 5
-  disk_type = "SSD"
-  bus = "VIRTIO"
+  server_id     = "${profitbricks_server.database.id}"
+  licence_type  = "OTHER"
+  name          = "data"
+  size          = 5
+  disk_type     = "SSD"
+  bus           = "VIRTIO"
 }
 
 resource "profitbricks_nic" "database_nic" {
-  datacenter_id = "${profitbricks_datacenter.main.id}"
-  server_id = "${profitbricks_server.database.id}"
-  lan = 2
-  dhcp = true
+  datacenter_id   = "${profitbricks_datacenter.main.id}"
+  server_id       = "${profitbricks_server.database.id}"
+  lan             = 2
+  dhcp            = true
   firewall_active = true
 }
 
 //resource "profitbricks_loadbalancer" "example" {
+
+
 //  datacenter_id = "${profitbricks_datacenter.example.id}"
+
+
 //  nic_id = "${profitbricks_nic.example.id}"
+
+
 //  name = "load balancer name"
+
+
 //  dhcp = true
+
+
 //}
+
--- example/variables.tf
+++ example/variables.tf
@@ -1,9 +1,17 @@
 variable "ubuntu" {
   description = "Ubuntu Server"
-  default = "ubuntu-16.04"
+  default     = "ubuntu-16.04"
 }
 
 variable "private_key_path" {
   description = "Path to file containing private key"
-  default = "/Users/{your_username}/.ssh/id_rsa"
+  default     = "/Users/{your_username}/.ssh/id_rsa"
+}
+
+variable "ssh_keys" {
+  description = "List of SSH Keys to be added to the VMs"
+
+  default = ["/home/{your_username}/.ssh/your_public_key",
+    "/home/ANOTHER-USER/.ssh/another_public_key",
+  ]
 }
