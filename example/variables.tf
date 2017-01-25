3efadc8d1342668b423282b114c3641dea3053f8
jasminSPC <jasmin@stackpointcloud.com>
2017-01-25 09:38:26 +0100

    Fixed variables.tf

--- example/variables.tf
+++ example/variables.tf
@@ -11,7 +11,7 @@ variable "private_key_path" {
 variable "ssh_keys" {
   description = "List of SSH Keys to be added to the VMs"
 
-  default = /["/home/{your_username}/.ssh/your_public_key",
+  default = ["/home/{your_username}/.ssh/your_public_key",
     "/home/ANOTHER-USER/.ssh/another_public_key",
   ]
 }
